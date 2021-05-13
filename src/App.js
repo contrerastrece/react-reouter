ff1a16b0435171b577ea01141a7f1db6af3adc5c
vcontreras <contrerastrece@gmail.com>
2021-05-12 23:53:48 -0500

    React Router, conceptos básicos

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import ConceptosBasicos from "./components/ConceptosBasicos";
+
+function App() {
+  return (
+    <div>
+      <h1>React Router</h1>
+      <a href="https://reactrouter.com/web/guides/quick-start" target="_blank" rel="noreferrer">documentacion</a>
+      <hr/>
+      <ConceptosBasicos/>
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ConceptosBasicos.js
@@ -0,0 +1,70 @@
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+const ConceptosBasicos = () => {
+  return (
+    <div>
+      <h2>Conceptos Basicos</h2>
+      {/* acá cargamos nuestras rutas */}
+      <Router>
+        {/* utilizaremos el componente Route */}
+
+        {/* en nuestra UI no mostrará todos los componentes que coincidan 
+          pro ejemplo al ingresar en la barra de navegacion 
+          localhost:300/contacto nos cargará home y contacto 
+        */}
+        {/* <Route path="/">
+          <h3>Home</h3>
+          <p>Bienvenido, esta es la pagina principal</p>
+        </Route>
+        <Route path="/acerca">
+          <h3>Acerca</h3>
+        </Route>
+        <Route path="/contacto">
+          <h3>Contacto</h3>
+        </Route> */}
+
+        {/* si queremos que nos carge el primero que coincida 
+        con nuestra ruta utilizaremos switch 
+        */}
+        {/* para este caso si en nuestra barra de navegacion escribimos 
+        localhost:300/contacto solo nos mostrará el contacto porque es el primero que coicide 
+        pero si nuestro componete home está ubicado primero nos mostrara siempre el home porque el / se encontró primero 
+        Se recomienda que vayamos de las más especificas a la más genericas para que no ocurra el error anterior
+        */}
+        {/* <Switch>
+        <Route path="/acerca">
+          <h3>Acerca</h3>
+        </Route>
+        <Route path="/contacto">
+          <h3>Contacto</h3>
+        </Route>
+        <Route path="/">
+          <h3>Home</h3>
+          <p>Bienvenido, esta es la pagina principal</p>
+        </Route>
+        </Switch> */}
+
+        {/* 
+         si tenemos nuestras rutas producto-android producto-apple
+         habría un comflico porque solo mostraría el primero que encuntre
+        para este caso usaremos exact y al momento de asignarle esta propiedad 
+        solo nos mostrará si es exactamente igual
+        */}
+        <Switch>
+          <Route exact path="/">
+            <h3>Home</h3>
+            <p>Bienvenido, esta es la pagina principal</p>
+          </Route>
+          <Route exact path="/acerca">
+            <h3>Acerca</h3>
+          </Route>
+          <Route exact path="/contacto">
+            <h3>Contacto</h3>
+          </Route>
+        </Switch>
+      </Router>
+    </div>
+  );
+};
+
+export default ConceptosBasicos;
